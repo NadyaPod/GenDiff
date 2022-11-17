17e06492b6517a9d744eee2677d11eb452dc3bb6
Nadya Pod <nadegda.podyacheva@gmail.com>
2022-11-17 17:30:11 +0700

    feat: commander arguments and format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,8 +6,9 @@ const program = new Command();
 program
   .name("genDiff")
   .description(" Compares two configuration files and shows a difference.")
-  .version("1.0.0");
+  .version("1.0.0")
+  .argument("<filepath1>")
+  .argument("<filepath2>")
+  .option("-f, --format <type>", "output format");
 
 program.parse();
-
-console.log("hello");
