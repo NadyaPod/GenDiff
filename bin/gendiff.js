84dfd34b01f81f83ff06db229a1558a1fc240d18
Nadya Pod <nadegda.podyacheva@gmail.com>
2022-11-21 18:44:33 +0700

    feat: gh actions workflow setup

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 
-import { Command } from "commander";
-import { getDiff } from "../src/compare.js";
+import { Command } from 'commander';
+import getDiff from '../src/compare.js';
+
 const program = new Command();
 
 program
-  .name("genDiff")
-  .description(" Compares two configuration files and shows a difference.")
-  .version("1.0.0")
-  .argument("<filepath1>")
-  .argument("<filepath2>")
-  .option("-f, --format <type>", "output format")
+  .name('genDiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>", "output format')
   .action((filepath1, filepath2) => getDiff(filepath1, filepath2));
 
 program.parse();
--- src/compare.js
+++ src/compare.js
@@ -1,46 +1,46 @@
-import fs from "fs";
-import path from "node:path/win32";
-import { cwd } from "node:process";
-import _ from "lodash";
+import fs from 'fs';
+import path from 'node:path/win32';
+import { cwd } from 'node:process';
+import _ from 'lodash';
 
-const minus = "-";
-const plus = "+";
-const eq = " ";
+const minus = '-';
+const plus = '+';
+const eq = ' ';
 
 const loadJSON = (filePath) => {
-  filePath = path.resolve(cwd(), filePath);
-  return JSON.parse(fs.readFileSync(filePath));
+  const fullPath = path.resolve(cwd(), filePath);
+  return JSON.parse(fs.readFileSync(fullPath));
 };
 
 const stringify = (diff) => {
   const strFormat = diff.map((item) => `  ${item[0]} ${item[1]}: ${item[2]}`);
-  return `{\n${strFormat.join("\n")}\n}`;
+  return `{\n${strFormat.join('\n')}\n}`;
 };
 
 const diff = (minuend, subtrahend) => {
-  const diff = [];
+  const difference = [];
 
   for (const [key, value] of Object.entries(minuend)) {
     if (Object.hasOwn(subtrahend, key)) {
       if (value === subtrahend[key]) {
-        diff.push([eq, key, value]);
+        difference.push([eq, key, value]);
       } else {
-        diff.push([minus, key, value], [plus, key, subtrahend[key]]);
+        difference.push([minus, key, value], [plus, key, subtrahend[key]]);
       }
     } else {
-      diff.push([minus, key, value]);
+      difference.push([minus, key, value]);
     }
   }
 
   for (const [key, value] of Object.entries(subtrahend)) {
     if (!Object.hasOwn(minuend, key)) {
-      diff.push(["+", key, value]);
+      difference.push(['+', key, value]);
     }
   }
-  return _.sortBy(diff, [(pair) => pair[1]]);
+  return _.sortBy(difference, [(pair) => pair[1]]);
 };
 
-export const getDiff = (filePath1, filePath2) => {
+export default (filePath1, filePath2) => {
   const minuend = loadJSON(filePath1);
   const subtrahend = loadJSON(filePath2);
   const strDiff = stringify(diff(minuend, subtrahend));
