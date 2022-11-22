b7b944e939542b67f115aa66973429a47534c50c
Nadya Pod <nadegda.podyacheva@gmail.com>
2022-11-22 19:53:09 +0700

    feat: impl generateDiff test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,16 +1,19 @@
-import difff from '../src/compare.js';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import fs from 'fs';
+
+import generateDiff from '../src/compare.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('diff', () => {
-  expect(difff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(`{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`);
-});
+  const expected = readFixture('expected.txt');
+  const file1Path = getFixturePath('file1.json');
+  const file2Path = getFixturePath('file2.json');
+  const actual = generateDiff(file1Path, file2Path);
 
-test('dud', () => {
-  expect(2).toBe(2);
+  expect(actual).toEqual(expected);
 });
